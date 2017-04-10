a8453397c5206e01e8a3050ef13f0d33284e1ba4
baddogdown <github@baddogdown.com>
2017-04-10 08:41:25 +0100

    upgrading hardcoded version

--- linuxToolSetup.sh
+++ linuxToolSetup.sh
@@ -8,7 +8,7 @@ cd ~
 if [ -d ~/google-cloud-sdk ];then
 	echo "google-cloud-sdk appears already installed, dont dowload.  If want to re-sintall delete the dir first."
 else
-	curl https://dl.google.com/dl/cloudsdk/channels/rapid/downloads/google-cloud-sdk-129.0.0-linux-x86_64.tar.gz > google-cloud-sdk.tar.gz
+	curl https://dl.google.com/dl/cloudsdk/channels/rapid/downloads/google-cloud-sdk-150.0.0-linux-x86_64.tar.gz > google-cloud-sdk.tar.gz
 	tar -zxvf google-cloud-sdk.tar.gz
 	~/google-cloud-sdk/install.sh
 fi
