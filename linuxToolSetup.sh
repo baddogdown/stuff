db5e6c856e5f88209a147aad22971a701ebfb529
baddogdown <github@baddogdown.com>
2017-07-19 12:07:44 -0700

    general refresh

--- linuxToolSetup.sh
+++ linuxToolSetup.sh
@@ -2,7 +2,7 @@
 
 # get the basics
 sudo apt-get update
-sudo apt-get -y install git mysql-client-5.6 curl ant make python git openjdk-7-jdk
+sudo apt-get -y install git mariadb-client curl ant make python git ##openjdk-7-jdk
 
 cd ~
 if [ -d ~/google-cloud-sdk ];then
