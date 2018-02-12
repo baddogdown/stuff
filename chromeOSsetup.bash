0ce8112da10216f4f6591e1b9ddd8ffbd2baaa45
Dan Gardner <github@baddogdown.com>
2018-02-12 14:34:30 +0000

    Various updates

--- chromeOSsetup.bash
+++ chromeOSsetup.bash
@@ -1,9 +1,21 @@
 #!/bin/bash
 
-curl https://raw.githubusercontent.com/dnschneid/crouton/master/installer/crouton > /home/chronos/user/Downloads/crouton
-sudo bash -e /home/chronos/user/Downloads/crouton -r xenial -t audio,cli-extra,core,extension,keyboard,touch,unity,x11,xiwi
+DIR=/home/chronos/user
 
-curl https://raw.githubusercontent.com/baddogdown/stuff/master/linuxToolSetup.sh > /home/chronos/user/linuxToolSetup.sh
-sudo bash -e /home/chronos/user/linuxToolSetup.sh
+curl https://raw.githubusercontent.com/dnschneid/crouton/master/installer/crouton > $DIR/crouton
+sudo bash -e $DIR/crouton -u -r xenial -t audio,cli-extra,core,extension,keyboard,touch,unity,x11,xiwi
+
+curl https://raw.githubusercontent.com/baddogdown/stuff/master/linuxToolSetup.sh > $DIR/linuxToolSetup.sh
+
+echo ""
+echo "---------------------------------------"
+echo "Entering new chroot ENV.  You will probably want to run the following once in there..."
+echo ""
+echo "bash -e ./linuxToolSetup.sh"
+echo ""
+echo ""
+echo ""
+sudo enter-chroot
+
 
 exit 0
--- linuxToolSetup.sh
+++ linuxToolSetup.sh
@@ -13,33 +13,62 @@ else
 	~/google-cloud-sdk/install.sh
 fi
 
-. ~/.bashrc
+. ~/.bashrc;
+. ~/.bashrc;
 
 gcloud --quiet components update
 gcloud --quiet components install kubectl
 
-
 # Run gcloud init to get started
 gcloud init
 
-mkdir ~/repos
+if [ ! -d ~/repos ];then
+	mkdir ~/repos
+fi
 cd ~/repos
 
 echo ""
-echo "Enter your github login/email"; read GITHUB_USER
+echo "--------------------------------------"
+echo "Setting up github stuff init"
+echo ""
+read -p "Enter your github login/email: " GITHUB_EMAIL
+echo ""
+read -p "Enter a username (to use in github commits): " GITHUB_USER
+echo ""
 
-ssh-keygen -t rsa -b 4096 -C "$GITHUB_USER"
-
+read -p "Do you want to (re)create ssh keys ? [y|anythnig else for no]" CREATE_SSHKEYS
+if [ "$CREATE_SSHKEYS" = "y" ];then
+	echo ""
+	echo "Creating ssh keys.  You need to upload these as a new public key on github agaisnt the account: $GITHUB_EMAIL"
+	echo ""
+	ssh-keygen -t rsa -b 4096 -C "$GITHUB_EMAIL"
+fi
 echo ""
 echo "Make sure you have added your github public key created above to your guithub account...[press return to continue]"; read CONTINUE
 
-for REPO in efundamentals/release-management efundamentals/internal-tools
+echo ""
+echo "Setting up git config --global user.email"
+git config --global user.email "$GITHUB_EMAIL"
+
+echo ""
+echo "Setting up git config --global user.name"
+git config --global user.name "$GITHUB_USER"
+echo ""
+echo ""
+
+echo ""
+echo "Cloning some repos"
+echo ""
+for REPO in efundamentals/release-management efundamentals/internal-tools baddogdown/stuff
 do
 
+	echo ""
+	echo "--------------------------------------"
+	echo "Cloning $REPO"
+	echo ""
 	git clone git@github.com:$REPO.git
 
 done
 
 
-git clone
 exit 0
