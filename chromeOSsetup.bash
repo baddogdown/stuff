31b683e6d7d84c96621949b334a35a8b684329f8
baddogdown <github@baddogdown.com>
2018-02-12 13:04:08 +0000

    make more fault tollerant of path

--- chromeOSsetup.bash
+++ chromeOSsetup.bash
@@ -1,9 +1,9 @@
 #!/bin/bash
 
-curl https://goo.gl/fd3zc > ~/Downloads/crouton
-sudo bash -e ~/Downloads/crouton -r xenial -t audio,cli-extra,core,extension,keyboard,touch,unity,x11,xiwi
+curl https://raw.githubusercontent.com/dnschneid/crouton/master/installer/crouton > /home/chronos/user/Downloads/crouton
+sudo bash -e /home/chronos/user/Downloads/crouton -r xenial -t audio,cli-extra,core,extension,keyboard,touch,unity,x11,xiwi
 
-curl https://raw.githubusercontent.com/baddogdown/stuff/master/linuxToolSetup.sh > ~/linuxToolSetup.sh
-sudo bash -e ~/linuxToolSetup.sh
+curl https://raw.githubusercontent.com/baddogdown/stuff/master/linuxToolSetup.sh > /home/chronos/user/linuxToolSetup.sh
+sudo bash -e /home/chronos/user/linuxToolSetup.sh
 
 exit 0
